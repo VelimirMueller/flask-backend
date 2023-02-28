e5bc886d5e3b36023eb6051669341a0d7b1908d9
unknown <velimir.mueller@googlemail.com>
2023-02-28 20:14:33 +0100

    refactor messages
    - since I was not happy with the messages and the keys I have refactored
      the messages dictionary a little bit to be more precise with standard
      responses or messages

---
+++ src/messages/api_messages.py
@@ -0,0 +1,10 @@
+API_MESSAGES = {
+    'invalidRequest': 'could not load json data - json request not in valid (or valid stringified) json format',
+    'invalidSchema': 'Could not validate schema!',
+    'requestType': 'API-Request',
+    'requestError': 'Request error - received request which can not be processed!',
+    'schemaType': 'API-Schema',
+    'schemaError': 'Schema error - received schema which can not be processed!',
+    'validRequest': 'Request is valid and could be processed properly.',
+    'internalServerError': 'something went very wrong'
+}
