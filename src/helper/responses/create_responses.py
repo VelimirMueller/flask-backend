ec165d3f99e83ca52a3f97db3822afed2e4b7138
unknown <velimir.mueller@googlemail.com>
2023-02-28 23:34:23 +0100

    fix multiple types in type hints IDE problems by using Union in these cases

--- src/helper/responses/create_responses.py
+++ src/helper/responses/create_responses.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+from typing import Union
 
 def return_success_response(successMessage:str, type:str, keysInRequest:int, schema:dict)->dict:
     # Returns a normalized success response in json format. This way the shema of responses are always the same which should be a big plus while developing.
@@ -11,7 +12,7 @@ def return_success_response(successMessage:str, type:str, keysInRequest:int, sch
         'statusCode': 200
     }
 
-def return_error_response(errorMessage:str, type:str='error type was not set', keysInRequest:str | int='not checked', schema:dict | str='not checked', statusCode:int=400)->dict:
+def return_error_response(errorMessage:str, type:str='error type was not set', keysInRequest:Union[int, str]='not checked', schema:Union[int, str]='not checked', statusCode:int=400)->dict:
     # Returns a normalized success response in json format. This way the shema of responses are always the same which should be a big plus while developing.
     return {
         'errorMessage': errorMessage,
@@ -22,7 +23,7 @@ def return_error_response(errorMessage:str, type:str='error type was not set', k
         'statusCode': statusCode
     }
 
-def return_error_response_dev(errorMessage:str, type:str='error type was not set', keysInRequest:str | int='not checked', schema:str | bool='not checked', statusCode:int=400, isDevMode:bool=False, devDebuggingMessage:str | dict='dev mode not active - pls set DEV_MODE=1 in .flaskenv')->dict:
+def return_error_response_dev(errorMessage:str, type:str='error type was not set', keysInRequest:Union[int, str]='not checked', schema:Union[bool, str]='not checked', statusCode:int=400, isDevMode:bool=False, devDebuggingMessage:Union[bool, str]='dev mode not active - pls set DEV_MODE=1 in .flaskenv')->dict:
     # Returns like return_error_response an normalized response. The only difference occurs when setting DEV_MODE=1 in .flaskenv.
     # This way the response will contain a dev debugging message, containing the exception message
     if str(os.environ.get('DEV_MODE')) == "1":
