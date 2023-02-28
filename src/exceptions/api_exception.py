dcc6e8986fe24cc578976b66e9ad22fb11191872
unknown <velimir.mueller@googlemail.com>
2023-02-28 23:36:14 +0100

    fix: use single instead of double quotes - was an accident anyways

--- src/exceptions/api_exception.py
+++ src/exceptions/api_exception.py
@@ -3,8 +3,8 @@ from src.exceptions.exception_error import exception_error
 def api_exception(error, statusCode):
   
   return {
-    "message": exception_error(error + ", statusCode: " + str(statusCode)),
-    "type": "api_exception",
-    "status": "error",
-    "statusCode": statusCode
+    'message': exception_error(error + ', statusCode: ' + str(statusCode)),
+    'type': 'api_exception',
+    'status': 'error',
+    'statusCode': statusCode
   }
