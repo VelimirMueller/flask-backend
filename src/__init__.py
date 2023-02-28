c731d4321853806addc1b1f7ea8d6327da982b5e
unknown <velimir.mueller@googlemail.com>
2023-02-28 23:23:57 +0100

    add comments to initialization file

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,17 @@
+# The Application initialization file
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# Basic Flask + SqlAlchemy initialization
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
+# Add routes and models here so the App has access to endpoints and database entities:
 from src.models import usermodel
 from src.routes import loginroute
